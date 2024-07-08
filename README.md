95149ddddb04337c99427efb48e2c676d006d187
Ian Meyer (imeyer) <k@imeyer.io>
2024-07-07 21:29:25 -0700

    Move sqlc to top-level directory
    - Updated sqlc.yaml to reflect new out dir
    - Updated README.md with better build instructions
    - Remove some unused tables from the schema

--- README.md
+++ README.md
@@ -3,15 +3,38 @@ Discussion board for your tailnet
 
 ## Why?
 
-Why not? Who doesn't want a discussion board for their tailnet? Honestly, I've often found myself wanting a small/simple/easy-to-deploy discussion board [a la pgBoard](https://github.com/pgBoard/pgBoard). The apps [golink](https://github.com/tailscale/golink) and [tclip](https://github.com/tailscale-dev/tclip) use the tailscale go library to expose the application securely on your tailnet. I thought this was a good fit for what I'm calling `tdiscuss`. That's why.
-
-## Running
-
-Coming soon...
+Why not? Who doesn't want a discussion board for their tailnet? Honestly, I've often found myself wanting a small/simple/easy-to-deploy discussion board [a la pgBoard](https://github.com/pgBoard/pgBoard). The apps [golink](https://github.com/tailscale/golink) and [tclip](https://github.com/tailscale-dev/tclip) use the tailscale go library to expose the application securely on your tailnet. I thought this was a good fit for what I'm calling `tdiscuss`. That's why. Well, and it's fun!
 
 ## Building
 
-1. Install [Bazelisk](https://github.com/bazelbuild/bazelisk#installation).
-1. `bazel build //cmd/tdiscuss:tdiscuss`
-1. Find the resulting binary in `$(bazelisk info bazel-bin)/cmd/tdiscuss/tdiscuss_/tdiscuss`
-1. Hope that it doesn't panic with `panic: godebug: Value of name not listed in godebugs.All:` as the error
+1. `go build -o tdiscuss cmd/tdiscuss/main.go`
+
+## Running
+
+1. Be a [tailscale](https://tailscale.com) user
+1. Have an [auth key](https://login.tailscale.com/admin/settings/keys) created for the last step in this list.
+1. Set up a PostgreSQL database version 14+
+1. `psql < sqlc/schema.sql`
+2. `DATABASE_URL=<valid dsn> TS_AUTHKEY=<key from step 2>`
+
+You should see some logs fly past until debugging gets turned off by default... they will look kinda like..
+```
+{
+  "time": "2024-07-07T19:49:31.283917-07:00",
+  "level": "INFO",
+  "msg": "tsnet running state path xxx/Library/Application Support/tailscale/discuss/tsnet/tailscaled.state"
+}
+{
+  "time": "2024-07-07T19:49:31.284866-07:00",
+  "level": "INFO",
+  "msg": "tsnet starting with hostname \"discuss\", varRoot \"xxx/Library/Application Support/tailscale/discuss/tsnet\""
+}
+```
+Success looks like
+```
+{
+  "time": "2024-07-07T19:49:37.305788-07:00",
+  "level": "INFO",
+  "msg": "AuthLoop: state is Running; done"
+}
+```
--- sqlc/schema.sql
+++ sqlc/schema.sql
@@ -5,22 +5,13 @@ CREATE TABLE board_data
   value   text NOT NULL CHECK(value <> ''), -- preference value
   UNIQUE(name)
 );
--- INSERT INTO board_data (name,value) VALUES ('total_members',0);
--- INSERT INTO board_data (name,value) VALUES ('total_threads',0);
--- INSERT INTO board_data (name,value) VALUES ('total_thread_posts',0);x
-
 
 CREATE TABLE member
 (
   id                   serial PRIMARY KEY,
-  -- name                 varchar(25) NOT NULL CHECK(name <> ''),     -- login name
-  -- pass                 char(32) NOT NULL CHECK(pass <> ''),        -- member password md5 hashed
-  -- secret               char(32) NOT NULL CHECK(secret <> ''),      -- secret word for password recovery md5 hashed
-  -- ip                   cidr NOT NULL,                              -- ip of member at last login
   date_joined          timestamptz DEFAULT now(),                    -- date of signup
   date_first_post       date,                                       -- the date of the member's first post
   email                varchar NOT NULL CHECK(email <> ''),    -- email used to sign up
-  -- postalcode           text NOT NULL CHECK(postalcode <> ''),      -- member's postalcode
   total_threads        int DEFAULT 0,                              -- member's total threads created
   total_thread_posts   int DEFAULT 0,                              -- member's total posts
   last_view            timestamp,                                  -- last view of board
@@ -69,31 +60,6 @@ CREATE TABLE pref
   ordering      int,
   UNIQUE(name)
 );
-
--- INSERT INTO pref VALUES (1,1,'photo','photo url',false,false,true,300,1);
--- INSERT INTO pref VALUES (2,1,'location','location',true,false,true,200,2);
--- INSERT INTO pref VALUES (3,1,'email','email',true,false,true,200,3);
--- INSERT INTO pref VALUES (4,1,'website','website',true,false,true,200,4);
--- INSERT INTO pref VALUES (5,1,'aim','aim',true,false,true,NULL,5);
--- INSERT INTO pref VALUES (6,1,'msn','msn',true,false,true,NULL,6);
--- INSERT INTO pref VALUES (7,1,'yahoo','yahoo',true,false,true,NULL,7);
--- INSERT INTO pref VALUES (8,1,'gtalk','gtalk',true,false,true,NULL,8);
--- INSERT INTO pref VALUES (9,1,'jabber','jabber',true,false,true,NULL,9);
--- INSERT INTO pref VALUES (10,3,'info','info',true,false,true,NULL,10);
--- INSERT INTO pref VALUES (11,2,'show_email','show email',false,false,true,NULL,12);
--- INSERT INTO pref VALUES (12,2,'hidemedia','hide media',false,true,true, NULL,13);
--- INSERT INTO pref VALUES (13,2,'ignore','soft ignore',false,true,true,NULL,14);
--- INSERT INTO pref VALUES (14,2,'nocollapse','disable collapsing',false,true,true,NULL,19);
--- INSERT INTO pref VALUES (15,3,'theme','theme',false,true,false,NULL,23);
--- INSERT INTO pref VALUES (17,2,'nofirstpost','hide firstpost arrow',false,true,true,NULL,15);
--- INSERT INTO pref VALUES (18,2,'italicread','italicize read posts',false,true,true,NULL,20);
--- INSERT INTO pref VALUES (19,2,'nopostnumber','hide posts #',false,true,true,NULL,21);
--- INSERT INTO pref VALUES (20,2,'notabs','hide nav tabs',false,true,true,NULL,22);
--- INSERT INTO pref VALUES (21,1,'mincollapse','<span class=''small''>min post # to collapse</span>',false,true,true,50,16);
--- INSERT INTO pref VALUES (22,1,'collapseopen','<span class=''small''># open after collapse (min 1)</span>',false,true,true,50,17);
--- INSERT INTO pref VALUES (23,1,'externalcss','external css<br/><span class=''small''>(may break color schemes)</span>',false,true,true,300,11);
--- INSERT INTO pref VALUES (24,1,'uncollapsecount','<span class=''small''># posts to uncollapse</span>',false,true,true,50,18);
-
 
 CREATE TABLE member_pref
 (
@@ -190,45 +156,6 @@ CREATE TABLE chat
   member_id  int NOT NULL,
   stamp      timestamp DEFAULT now(),
   chat       text
-);
-
-CREATE TABLE theme
-(
-  id      serial PRIMARY KEY,
-  name    text NOT NULL CHECK(name <> ''),
-  value   text,
-  main    bool NOT NULL DEFAULT false,
-  UNIQUE(name)
-);
-
--- INSERT INTO theme (main,name,value) VALUES (true,'blue','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#333333";s:4:"even";s:7:"#c3dae4";s:3:"odd";s:7:"#acccdb";s:2:"me";s:7:"#82b3c9";s:5:"hover";s:7:"#82b3c9";s:7:"readbar";s:7:"#3488ab";s:4:"menu";s:7:"#555555";}');
--- INSERT INTO theme (name,value) VALUES ('simple','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#ffffff";s:4:"even";s:7:"#cccccc";s:3:"odd";s:7:"#eeeeee";s:2:"me";s:7:"#82b3c9";s:5:"hover";s:7:"#82b3c9";s:7:"readbar";s:7:"#82b3c9";s:4:"menu";s:7:"#555555";}');
--- INSERT INTO theme (name,value) VALUES ('gray','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#555555";s:4:"even";s:7:"#d7d7d7";s:3:"odd";s:7:"#c9c9c9";s:2:"me";s:7:"#adadad";s:5:"hover";s:7:"#adadad";s:7:"readbar";s:7:"#333333";s:4:"menu";s:7:"#555555";}');
--- INSERT INTO theme (name,value) VALUES ('white','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#ffffff";s:4:"even";s:7:"#cccccc";s:3:"odd";s:7:"#eeeeee";s:2:"me";s:7:"#999999";s:5:"hover";s:7:"#999999";s:7:"readbar";s:7:"#666666";s:4:"menu";s:7:"#555555";}');
--- INSERT INTO theme (name,value) VALUES ('black','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#000000";s:4:"even";s:7:"#bbbbbb";s:3:"odd";s:7:"#dddddd";s:2:"me";s:7:"#666666";s:5:"hover";s:7:"#666666";s:7:"readbar";s:7:"#555555";s:4:"menu";s:7:"#000000";}');
--- INSERT INTO theme (name,value) VALUES ('purple','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#333333";s:4:"even";s:7:"#bebde9";s:3:"odd";s:7:"#a6a5e1";s:2:"me";s:7:"#7978d2";s:5:"hover";s:7:"#7978d2";s:7:"readbar";s:7:"#5553ae";s:4:"menu";s:7:"#555555";}');
--- INSERT INTO theme (name,value) VALUES ('green','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#333333";s:4:"even";s:7:"#d4f0be";s:3:"odd";s:7:"#c5eba7";s:2:"me";s:7:"#a8e07a";s:5:"hover";s:7:"#a8e07a";s:7:"readbar";s:7:"#3e8c00";s:4:"menu";s:7:"#555555";}');
--- INSERT INTO theme (name,value) VALUES ('orange','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#333333";s:4:"even";s:7:"#e0c18b";s:3:"odd";s:7:"#dbb878";s:2:"me";s:7:"#d1a453";s:5:"hover";s:7:"#d1a453";s:7:"readbar";s:7:"#a36d00";s:4:"menu";s:7:"#555555";}');
--- INSERT INTO theme (name,value) VALUES ('red','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#333333";s:4:"even";s:7:"#a22626";s:3:"odd";s:7:"#ae2929";s:2:"me";s:7:"#7e0101";s:5:"hover";s:7:"#7e0101";s:7:"readbar";s:7:"#111111";s:4:"menu";s:7:"#555555";}');
--- INSERT INTO theme (name,value) VALUES ('halloween','a:9:{s:4:"font";s:37:"verdana, helvetica, arial, sans-serif";s:8:"fontsize";s:3:"1.1";s:4:"body";s:7:"#333333";s:4:"even";s:7:"#eaa61e";s:3:"odd";s:7:"#f4b028";s:2:"me";s:7:"#000000";s:5:"hover";s:7:"#000000";s:7:"readbar";s:7:"#a36d00";s:4:"menu";s:7:"#555555";}');
-
-CREATE TABLE fundraiser
-(
-  id     serial PRIMARY KEY,
-  name   text,
-  goal   money
-);
-
-CREATE TABLE donation
-(
-  id              serial PRIMARY KEY,
-  fundraiser_id   int NOT NULL REFERENCES fundraiser(id),
-  payment_date    date NOT NULL DEFAULT now(),
-  payment_status  text,
-  payer_email     text,
-  txn_id          text,
-  payment_fee     money,
-  payment_gross   money
 );
 
 CREATE OR REPLACE FUNCTION member_sync() RETURNS trigger AS $$
