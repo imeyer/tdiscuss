43ebbde47d5c9789782a78bc3d9ba6a0f478e04b
Ian Meyer <k@imeyer.io>
2024-10-12 21:47:53 +0000

    Remove check-go-versions footgun
    Change-Id: I012276ecafd391899026882da5ecbeed49ee55c7
    Signed-off-by: Ian Meyer <k@imeyer.io>

--- check-go-versions.sh
+++ check-go-versions.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
+set -x
 # Everything should match what's in go.mod
 GV=$(grep "^go" go.mod | awk '{print $2}' | tr -d 'v')
 
 # There are only two places where version is defined so far but this should
 # catch most others as they're added as 'go-version'
-readarray -t versions < <(grep -h -r --include="*.yml" --include="*.yaml" go-version . | \
+readarray -t versions < <(grep -h -r --include=*.yml --include=*.yaml -E '^\s+go-version' . | \
                           awk '{print $2}' | \
                           tr -d "'" | \
                           uniq)
